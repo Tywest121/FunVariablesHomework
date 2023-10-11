a70329dad1e9a8b4656210f2ca6a7ab3f7fd049b
tywest121 <tyresewest25@gmail.com>
2023-10-11 18:14:55 -0400

    Westly Mcmuffin

---
+++ app.js
@@ -0,0 +1,26 @@
+var a = 2
+var b = 4
+var c = 6
+var d = 8
+var e = 10
+var f = 12
+var g = 14
+var h = 16
+var i = 18
+var j = 20
+console.log(a+b);
+console.log(c+d);
+console.log(e-f);
+console.log(g-h);
+console.log(i*j);
+console.log(d*g);
+console.log(i/c);
+console.log(j/b);
+const y = f++;
+console.log(`f:${f}, y:${y}`);
+const z = i++
+console.log(`i:${i}, z:${z}`);
+const r = g--;
+console.log(`g:${g}, r:${r}`);
+const s = h--;
+console.log(`h:${h}, s:${s}`);
